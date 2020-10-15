468e238d843e5e61bcb8503a64e571bb1645595e
Gimhan Alwis <gimhan.kalahra4@gmail.com>
2020-10-15 21:22:59 +0530

    update

---
+++ scroll/gkayscroll.js
@@ -0,0 +1,32 @@
+<script type="text/javascript">
+          jQuery(document).ready(function() {
+	       jQuery('.left').addClass("hidden").viewportChecker({
+               classToAdd: 'visible animated fadeInLeftBig', // Class to add to the elements when they are visible
+	           offset: 100    
+	       });   
+          });            
+      </script>
+      <script type="text/javascript">
+          jQuery(document).ready(function() {
+	       jQuery('.right').addClass("hidden").viewportChecker({
+               classToAdd: 'visible animated fadeInRightBig', // Class to add to the elements when they are visible
+	           offset: 100    
+	       });   
+          });            
+      </script>
+      <script type="text/javascript">
+          jQuery(document).ready(function() {
+	       jQuery('.fade').addClass("hidden").viewportChecker({
+               classToAdd: 'visible animated fadeInUpBig', // Class to add to the elements when they are visible
+	           offset: 100    
+	       });   
+          });            
+      </script>
+      <script type="text/javascript">
+          jQuery(document).ready(function() {
+	       jQuery('.zoom').addClass("hidden").viewportChecker({
+               classToAdd: 'visible animated bounceIn', // Class to add to the elements when they are visible
+	           offset: 100    
+	       });   
+          });            
+      </script>
